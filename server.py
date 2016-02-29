cb1edbd80953adcc001d2a1defa9ff0660311ae1
thebitstick <thebitstick@tfwno.gf>
2016-02-29 13:01:43 -0600

    Fixes

--- server.py
+++ server.py
@@ -1,54 +1,32 @@
 # Imports
 import psutil
 import platform
-import socket
-import os
 from flask import Flask, render_template, url_for, send_from_directory
-from flask import Markup
 import threading
 
 # Operating System
-osprint = platform.system()
+os = platform.system()
 release = platform.release()
-
-# Operating System Print
-os1 = 'OS: ' + osprint
-os2 = 'Release: ' + release
-os = os1 + '<br>' + os2
 
 # Hostname
 hostname = platform.node()
-host = 'Hostname : ' + hostname
 
 # Memory Variables
-mem = psutil
-virtual = mem.virtual_memory()
-total = float(virtual.total) / 1000000
-used = float(virtual.used) / 1000000
-free = float(virtual.free) / 1000000
-available = float(virtual.available) / 1000000
-per = float(virtual.percent)
-RAM1 = 'Total RAM: ' + str(round(total)) + 'MB<br>'
-RAM2 = 'Used RAM: ' + str(round(used)) + 'MB<br>'
-RAM3 = 'Free RAM: ' + str(round(free)) + 'MB<br>'
-RAM4 = 'Available RAM: ' + str(round(available)) + 'MB<br>'
-RAM5 = 'RAM: ' + str(per) + '%'
-RAM = RAM1 + RAM2 + RAM3 + RAM4 + RAM5
+virtual = psutil.virtual_memory()
+memory = str(float(virtual.percent)) + 'MB'
+RAM = str(per) + '%'
 
 # CPU Variables
 cpu = psutil
 percent = cpu.cpu_percent()
-count = cpu.cpu_count()
-processor1 = 'CPU usage: ' + str(round(percent)) + '%<br>'
-processor2 = 'CPU count: ' + str(round(count)) + '%'
-processor = processor1 + processor2
+processor = str(round(percent)) + '%'
 
 # Flask
 app = Flask(__name__)
 
 @app.route('/')
 def site():
-  return render_template('return.html', os=os, host=host, RAM=RAM, processor=processor)
+  return render_template('return.html', os=os, release=release, memory=memory, host=hostname, RAM=RAM, processor=processor)
 
 if __name__ == '__main__':
   app.run(port=9111)
